c61bb621a78f765cc620bbe9dfa3b67ed6da5fe3
soruboruit <113847544+soruboruit@users.noreply.github.com>
2022-10-05 17:24:52 +0900

    学校でやりました

---
+++ schol.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int y, m, d;
+    y = 2022;
+    m = 10;
+    d = 5;
+    printf("今日%d年%d月%d日\n", y, m, d);
+
+    y = 2005;
+    m = 5;
+    d = 26;
+    printf("私の生年月日は%d年%d月%d日です.\n", y, m, d);
+
+    return 0;
+}
